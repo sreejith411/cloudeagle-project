914f12bffba99f942638451997f0c085b06d15ee
sreejith411 <sreejith2604@gmail.com>
2026-05-06 06:18:55 +0000

    Add env-based response

--- spring-app/src/main/java/com/demo/spring_app/HelloController.java
+++ spring-app/src/main/java/com/demo/spring_app/HelloController.java
@@ -1,5 +1,6 @@
 package com.demo.spring_app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -8,18 +9,43 @@ public class HelloController {
 
     private final UserRepository repo;
 
+    // Read ENV from Cloud Run
+    @Value("${ENV:local}")
+    private String env;
+
+    // Optional: build number for debugging
+    @Value("${BUILD_NUMBER:unknown}")
+    private String build;
+
     public HelloController(UserRepository repo) {
         this.repo = repo;
     }
 
     @GetMapping("/")
     public String home() {
-        return "Hello world 🚀";
+
+        String message = "";
+
+        if (env.equalsIgnoreCase("qa")) {
+            message = "🧪 QA Environment - Sync Service Running";
+        } else if (env.equalsIgnoreCase("staging")) {
+            message = "🟡 STAGING Environment - Pre-Production";
+        } else if (env.equalsIgnoreCase("prod")) {
+            message = "🚀 PRODUCTION Environment - Live System";
+        } else {
+            message = "💻 LOCAL Environment";
+        }
+
+        return message + " | Build: " + build;
     }
 
     @GetMapping("/db-test")
     public String testDB() {
-        return "DB Connected Successfully 🚀";
-}
+        return "DB Connected Successfully in " + env.toUpperCase() + " 🚀";
+    }
 
+    @GetMapping("/health")
+    public String health() {
+        return "OK - " + env.toUpperCase();
+    }
 }
